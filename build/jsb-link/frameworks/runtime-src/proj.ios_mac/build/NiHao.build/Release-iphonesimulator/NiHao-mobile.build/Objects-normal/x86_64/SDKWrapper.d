11cfde8980b9fe668dc521d6f53fcaf2c63de4b1
Kuldeep Bhimte <arionlabs@Arions-Mac-mini.local>
2020-07-14 17:18:35 +0800

    removed submodules

---
+++ build/jsb-link/frameworks/runtime-src/proj.ios_mac/build/NiHao.build/Release-iphonesimulator/NiHao-mobile.build/Objects-normal/x86_64/SDKWrapper.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/arionlabs/Desktop/TestFramework/build/jsb-link/frameworks/runtime-src/proj.ios_mac/ios/Prefix.pch \
+  /Users/arionlabs/Desktop/TestFramework/build/jsb-link/frameworks/runtime-src/proj.ios_mac/ios/SDKWrapper.m \
+  /Users/arionlabs/Desktop/TestFramework/build/jsb-link/frameworks/runtime-src/proj.ios_mac/ios/SDKWrapper.h \
+  /Users/arionlabs/Desktop/TestFramework/build/jsb-link/frameworks/runtime-src/proj.ios_mac/ios/service/SDKDelegate.h
---
+++ build/jsb-link/frameworks/runtime-src/proj.ios_mac/build/SharedPrecompiledHeaders/SharedPrecompiledHeaders/7937661798457108990/Prefix.pch.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/arionlabs/Desktop/TestFramework/build/jsb-link/frameworks/runtime-src/proj.ios_mac/ios/Prefix.pch
